234605ea875554ef25c99f163cdce5a12d020531
JeanneBarrirere <barriere.jeanne@gmail.com>
2020-12-18 14:28:57 +0100

    apparence interface mac

--- src/pijakogui/MyButton.java
+++ src/pijakogui/MyButton.java
@@ -75,6 +75,7 @@ public class MyButton extends JButton {
             public void mouseEntered(java.awt.event.MouseEvent evt) { }
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 //client.Client.getInstance().publishSmiley("smile.png");
+                Service.updateUsersConnected(new String[]{"Jeanne"}, "Team Violetta");
             }
             public void mouseExited(java.awt.event.MouseEvent evt) { }
         });
